28507d52a4f0b92f428e9e8e1bc0ba438d06e63c
nandhinic2795 <30898117+nandhinic2795@users.noreply.github.com>
2017-08-24 23:22:27 +0530

    Add files via upload

---
+++ es3.js
@@ -0,0 +1,31 @@
+function checkLetter(obj){
+                chr = obj.value;
+                obj.value = '';
+                for(var i=0; i < secWord.length; i++){
+                    if(secWord.charAt(i) == chr.toLowerCase()){
+                        oSecLetters[i].style.backgroundColor = 'cyan';
+                        oSecLetters[i].innerHTML = chr;
+                    }
+                }
+            }
+            window.onload=function(){
+                secWord = 'micromax';
+                var oSecretWordContainer = document.getElementById('secretWordContainer');
+                for(var i=0; i < secWord.length; i++){
+                    var newDiv = document.createElement('div');
+                    newDiv.className = 'secLetters';
+                    oSecretWordContainer.appendChild(newDiv);
+                }
+                oSecLetters = oSecretWordContainer.getElementsByTagName('div');
+                document.getElementById('txtLetter').onkeyup = function(){
+                    checkLetter(this);
+                }
+                document.getElementById('txtLetter').focus();
+                document.getElementById('btnReset').onclick=function(){
+                    for(var i=0; i < secWord.length; i++){
+                        oSecLetters[i].style.backgroundColor = 'white';
+                        oSecLetters[i].innerHTML = '';
+                    }
+                    document.getElementById('txtLetter').focus();
+                }
+            }
